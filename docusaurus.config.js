a4aae20a320e0a042e7c9ad01580b29208b76819
lansolo99 <lansolo99@hotmail.fr>
2021-06-28 09:41:18 +0200

    Set doc-only mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,10 +80,12 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
             'https://github.com/lansolo99/s-money/edit/main/',
         },
+        
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
