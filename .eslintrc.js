5efd20cdf5f0f26bd1c5bd507c11ee95a5e17ab7
lansolo99 <lansolo99@hotmail.fr>
2021-06-30 09:46:46 +0200

    Add eslint+prettier configs

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@imaginary-cloud/react'],
+  parser: "babel-eslint",
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    'import/no-unresolved': [
+      ERROR,
+      { ignore: ['^@theme', '^@docusaurus', '^@generated'] },
+    ],
+    'import/extensions': OFF,
+  },
+}
--- src/theme/UseCase/index.js
+++ src/theme/UseCase/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import styles from './use-case.module.css';
+import React from 'react'
+import styles from './use-case.module.css'
 
-const UseCase = ({children}) => {
-    return ( <div className={styles.container}>
-        {/* Col */}
-        <div className={`${styles.col} ${styles.colL}`}>
-            {children}
-            </div>
-            {/* Col */}
-        <div className={`${styles.col} ${styles.colR}`}>
-            Code snippet here
-        </div>
-            
-        </div> );
+const UseCase = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      {/* Col */}
+      <div className={`${styles.col} ${styles.colL}`}>{children}</div>
+      {/* Col */}
+      <div className={`${styles.col} ${styles.colR}`}>Code snippet here</div>
+    </div>
+  )
 }
- 
+
 export default UseCase
