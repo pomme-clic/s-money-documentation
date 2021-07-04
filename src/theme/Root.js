bb3191737b499ad5d1068ed776a2c256f8857fa7
lansolo99 <lansolo99@hotmail.fr>
2021-07-04 17:31:03 +0200

    Add custom API style for /api routes

---
+++ src/theme/Root.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+const Root = ({ children }) => {
+  const { pathname } = useLocation()
+  const apiStyle =
+    pathname.includes('/api') && !pathname.includes('introduction')
+
+  return (
+    <>
+      <div className={apiStyle && 'api'}>{children}</div>
+    </>
+  )
+}
+
+export default Root
