40959a8c54e69166f6490e3d66187c10450d1bd0
lansolo99 <lansolo99@hotmail.fr>
2021-07-04 16:15:05 +0200

    Set working SSR rapidoc + set netlify cli linking

--- src/theme/Rapidoc/index.js
+++ src/theme/Rapidoc/index.js
@@ -1,47 +1,42 @@
-import React, { useRef, useLayoutEffect } from 'react'
-import 'rapidoc'
+import React, { useRef, useState, useLayoutEffect } from 'react'
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
 
 const Rapidoc = ({ apiUrl, children }) => {
-  const rapidocNode = useRef()
+  const [displayDoc, setDisplayDoc] = useState(false)
 
   useLayoutEffect(() => {
-    if (typeof window !== 'undefined') {
-      // const RapidocUI = require('rapidoc')
-      // RapidocUI({
-      //   domNode: rapidocNode.current,
-      //   ['spec-url']: 'https://petstore.swagger.io/v2/swagger.json',
-      //   ['render-style']: 'read',
-      // })
-      // SwaggerUI({
-      //   domNode: swaggerNode.current,
-      //   spec: localAPI,
-      // })
+    if (ExecutionEnvironment.canUseDOM) {
+      require('rapidoc')
+      setDisplayDoc(true)
+      console.log('can execute DOM')
     }
   }, [])
 
   return (
-    <div ref={rapidocNode}>
-      {/* {children}
-      <rapi-doc
-        spec-url={apiUrl}
-        render-style="read"
-        show-header="false"
-        show-info="false"
-        allow-search="false"
-        allow-advanced-search="false"
-        allow-api-list-style-selection="false"
-        style={{ height: '100vh', width: '100%' }}
-      >
-        <p> This is an example of adding external HTML content. </p>
-        <p> You may add: </p>
-        <ul>
-          <li> Tables </li>
-          <li> Text </li>
-          <li> Images </li>
-          <li> Links </li>
-          <li> Any HTML content </li>
-        </ul>
-      </rapi-doc> */}
+    <div>
+      {children}
+      {displayDoc && (
+        <rapi-doc
+          spec-url={apiUrl}
+          render-style="read"
+          show-header="false"
+          show-info="false"
+          allow-search="false"
+          allow-advanced-search="false"
+          allow-api-list-style-selection="false"
+          style={{ height: '100vh', width: '100%' }}
+        >
+          <p> This is an example of adding external HTML content. </p>
+          <p> You may add: </p>
+          <ul>
+            <li> Tables </li>
+            <li> Text </li>
+            <li> Images </li>
+            <li> Links </li>
+            <li> Any HTML content </li>
+          </ul>
+        </rapi-doc>
+      )}
     </div>
   )
 }
