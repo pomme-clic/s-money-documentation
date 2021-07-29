f909926e08e9361d4969af053f4c46762763c95c
lansolo99 <lansolo99@hotmail.fr>
2021-07-29 08:18:02 +0200

    Clear build cache & cleanup rapidoc conf

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -6,7 +6,6 @@ const queryClient = new QueryClient()
 
 import { useLocation } from 'react-router-dom'
 import clsx from 'clsx'
-import styles from './root.module.css'
 
 const Root = ({ children }) => {
   const { pathname } = useLocation()
@@ -22,7 +21,6 @@ const Root = ({ children }) => {
         <div
           className={clsx(
             'smoney selection:bg-xp-primary-500 selection:text-black',
-            styles.smoney,
             isApiEmbeddingPage ? 'api' : '',
             {
               homepage: isDocHomepage,
