6aaa3300531be5638df21021a3b664f3d848204f
lansolo99 <lansolo99@hotmail.fr>
2021-07-30 10:59:25 +0200

    Set better homepage features darkmode style

--- src/theme/DocHomepage/feature.js
+++ src/theme/DocHomepage/feature.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './feature.module.css'
 
 const Feature = ({ children, title, icon }) => {
   return (
     <>
-      <div className={`bg-white rounded-lg shadow-subtle p-10 space-y-4`}>
+      <div className={` ${styles.feature}`}>
         <div className="flex items-center justify-center w-16 h-16 p-4 rounded-full bg-xp-primary-500">
           <img src={`/img/ui/icons/${icon}.svg`} />
         </div>
