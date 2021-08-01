dee6e6f12b793fdd7095f3177cfb434bf04b15f0
lansolo99 <lansolo99@hotmail.fr>
2021-08-01 09:24:46 +0200

    Start implementing search autocomplete custom styles

--- plugins/@easyops-cn/docusaurus-search-local/dist/client/client/theme/SearchBar/icons.js
+++ plugins/@easyops-cn/docusaurus-search-local/dist/client/client/theme/SearchBar/icons.js
@@ -1,7 +1,22 @@
-export const iconTitle = '<svg width="20" height="20" viewBox="0 0 20 20"><path d="M17 6v12c0 .52-.2 1-1 1H4c-.7 0-1-.33-1-1V2c0-.55.42-1 1-1h8l5 5zM14 8h-3.13c-.51 0-.87-.34-.87-.87V4" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linejoin="round"></path></svg>';
-export const iconHeading = '<svg width="20" height="20" viewBox="0 0 20 20"><path d="M13 13h4-4V8H7v5h6v4-4H7V8H3h4V3v5h6V3v5h4-4v5zm-6 0v4-4H3h4z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
-export const iconContent = '<svg width="20" height="20" viewBox="0 0 20 20"><path d="M17 5H3h14zm0 5H3h14zm0 5H3h14z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linejoin="round"></path></svg>';
-export const iconAction = '<svg width="20" height="20" viewBox="0 0 20 20"><g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M18 3v4c0 2-2 4-4 4H2"></path><path d="M8 17l-6-6 6-6"></path></g></svg>';
-export const iconNoResults = '<svg width="40" height="40" viewBox="0 0 20 20" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path d="M15.5 4.8c2 3 1.7 7-1 9.7h0l4.3 4.3-4.3-4.3a7.8 7.8 0 01-9.8 1m-2.2-2.2A7.8 7.8 0 0113.2 2.4M2 18L18 2"></path></svg>';
-export const iconTreeInter = '<svg viewBox="0 0 24 54"><g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M8 6v42M20 27H8.3"></path></g></svg>';
-export const iconTreeLast = '<svg viewBox="0 0 24 54"><g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M8 6v21M20 27H8.3"></path></g></svg>';
+export const iconTitle =
+  '<svg width="20" height="20" viewBox="0 0 20 20"><path d="M17 6v12c0 .52-.2 1-1 1H4c-.7 0-1-.33-1-1V2c0-.55.42-1 1-1h8l5 5zM14 8h-3.13c-.51 0-.87-.34-.87-.87V4" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linejoin="round"></path></svg>'
+export const iconHeading =
+  '<svg width="20" height="20" viewBox="0 0 20 20"><path d="M13 13h4-4V8H7v5h6v4-4H7V8H3h4V3v5h6V3v5h4-4v5zm-6 0v4-4H3h4z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
+export const iconContent =
+  '<svg width="20" height="20" viewBox="0 0 20 20"><path d="M17 5H3h14zm0 5H3h14zm0 5H3h14z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linejoin="round"></path></svg>'
+export const iconAction = `<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24">
+  <defs/>
+  <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.42 13.92H15a4 4 0 004-4v-2"/>
+  <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8.41 9.93l-3.99 3.99 3.99 3.99"/>
+</svg>`
+export const iconNoResults = `<svg width="40" height="40" viewBox="0 0 23.8 23.8">
+  <defs/>
+  <path d="M11.9 23.8C5.3 23.8 0 18.4 0 11.9S5.3 0 11.9 0s11.9 5.3 11.9 11.9-5.4 11.9-11.9 11.9zm0-22C6.3 1.8 1.8 6.3 1.8 11.9S6.3 22 11.9 22 22 17.5 22 11.9 17.5 1.8 11.9 1.8z"/>
+  <path d="M14.3 15.7c-.2 0-.4-.1-.6-.3l-4.9-5c-.3-.3-.3-.9 0-1.2s.9-.3 1.2 0l5 4.9c.3.3.3.9 0 1.2-.2.3-.4.4-.7.4z"/>
+  <path d="M9.4 15.7c-.2 0-.4-.1-.6-.3-.3-.3-.3-.9 0-1.2l4.9-4.9c.3-.3.9-.3 1.2 0 .3.3.3.9 0 1.2l-5 5c-.1.1-.3.2-.5.2z"/>
+</svg>
+`
+export const iconTreeInter =
+  '<svg viewBox="0 0 24 54"><g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M8 6v42M20 27H8.3"></path></g></svg>'
+export const iconTreeLast =
+  '<svg viewBox="0 0 24 54"><g stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><path d="M8 6v21M20 27H8.3"></path></g></svg>'
