de3fdd82046bdbe44ab1c7a9d561bd40cded13d4
MaximeNoinin <maxime.noinin@s-money.fr>
2021-08-03 18:55:15 +0200

    Changement du menu

--- sidebars.js
+++ sidebars.js
@@ -3,28 +3,18 @@ module.exports = {
     'docs/home',
     {
       type: 'category',
-      label: 'Get started',
-      items: ['docs/get-started/doc1', 'docs/get-started/doc2'],
+      label: 'Account Holders',
+      items: ['docs/account-holders/doc1', 'docs/account-holders/doc2', 'docs/account-holders/doc3'],
     },
     {
       type: 'category',
-      label: 'SEPA transaction',
-      items: ['docs/sepa-transactions/doc1', 'docs/sepa-transactions/doc2'],
+      label: 'Cards',
+      items: ['docs/cards/doc1', 'docs/cards/doc2','docs/cards/doc3'],
     },
     {
       type: 'category',
-      label: 'Credit card',
-      items: ['docs/credit-card/doc1'],
-    },
-    {
-      type: 'category',
-      label: 'Payment',
-      items: ['docs/payment/doc1'],
-    },
-    {
-      type: 'category',
-      label: 'KYC',
-      items: ['docs/kyc/doc1'],
+      label: 'Make A Payement',
+      items: ['docs/make-a-payment/doc1', 'docs/make-a-payment/doc2', 'docs/make-a-payment/doc3', 'docs/make-a-payment/doc4'],
     },
   ],
   apisSidebar: [
