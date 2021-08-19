c9b371060736b42356d89ca25285292ef0cdc988
lansolo99 <lansolo99@hotmail.fr>
2021-08-19 15:55:40 +0200

    Inject Cookiebot using head w. errors

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Head from '@docusaurus/Head'
 
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
@@ -9,11 +10,25 @@ import clsx from 'clsx'
 
 const Root = ({ children }) => {
   const { pathname } = useLocation()
-
   const isDocPage = pathname.includes('/docs')
 
   return (
     <>
+      <Head>
+        <script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="dcbc9948-770f-4b0b-971c-d564f7143040"
+          data-blockingmode="auto"
+          type="text/javascript"
+        ></script>
+        <script
+          id="CookieDeclaration"
+          src="https://consent.cookiebot.com/dcbc9948-770f-4b0b-971c-d564f7143040/cd.js"
+          type="text/javascript"
+          defer
+        ></script>
+      </Head>
       <QueryClientProvider client={queryClient}>
         <div
           className={clsx(
@@ -25,6 +40,7 @@ const Root = ({ children }) => {
         >
           {children}
         </div>
+
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </>
