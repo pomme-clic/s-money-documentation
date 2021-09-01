b5c3b2f6049b1917be1084defbd2b6f54e4e849c
MaximeNoinin <maxime.noinin@s-money.fr>
2021-09-01 17:44:47 +0200

    account

--- sidebars.js
+++ sidebars.js
@@ -9,6 +9,16 @@ module.exports = {
     },
     {
       type: 'category',
+      label: 'Accounts',
+      items: ['docs/accounts/iban', 'docs/accounts/banking-mobility', 'docs/accounts/unpaid-operations', 'docs/accounts/account-management'],
+    },
+    {
+      type: 'category',
+      label: 'Onboard new customers',
+      items: ['docs/kyc/retail-customer', 'docs/kyc/identification', 'docs/kyc/business'],
+    },
+    {
+      type: 'category',
       label: 'Cards',
       items: ['docs/cards/doc1', 'docs/cards/doc2','docs/cards/doc3'],
     },
