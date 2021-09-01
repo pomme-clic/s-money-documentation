0c7381f7507c327e66439e296f79b59581fdca0a
MaximeNoinin <80476987+MaximeNoinin@users.noreply.github.com>
2021-09-01 18:28:57 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -9,18 +9,28 @@ module.exports = {
     },
     {
       type: 'category',
-      label: 'Onboard new customers 2',
-      items: ['docs/kyc/retail-customer', 'docs/kyc/identification', 'docs/kyc/business'],
+      label: 'Accounts',
+      items: ['docs/accounts/iban', 'docs/accounts/account-management', 'docs/accounts/unpaid-operations', 'docs/accounts/banking-mobility'],
     },
     {
       type: 'category',
       label: 'Cards',
-      items: ['docs/cards/doc1', 'docs/cards/doc2','docs/cards/doc3'],
+      items: ['docs/cards/issuing', 'docs/cards/card-management','docs/cards/x-pay'],
     },
     {
       type: 'category',
-      label: 'Payment',
-      items: ['docs/make-a-payment/doc1', 'docs/make-a-payment/doc2', 'docs/make-a-payment/doc3', 'docs/make-a-payment/doc4'],
+      label: 'Pay IN',
+      items: ['docs/payin/general', 'docs/payin/sdd', 'docs/payin/topup'],
+    },
+    {
+      type: 'category',
+      label: 'Pay OUT',
+      items: ['docs/payout/sct', 'docs/payout/beneficiaries', 'docs/payout/card-payment'],
+    },
+    {
+      type: 'category',
+      label: 'Customer Management',
+      items: ['docs/customer-relationship/account-statement', 'docs/customer-relationship/billing', 'docs/customer-relationship/reporting', 'docs/customer-relationship/business-portal'],
     },
   ],
   apisSidebar: [
