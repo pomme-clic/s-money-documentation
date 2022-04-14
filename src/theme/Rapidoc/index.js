9f107122580fabd5060fbe77fe2c51fc0d880ca5
MaximeNoinin <maxime.noinin@s-money.fr>
2022-04-14 10:21:29 +0200

    Update index.js

--- src/theme/Rapidoc/index.js
+++ src/theme/Rapidoc/index.js
@@ -155,7 +155,7 @@ const Rapidoc = ({ apiUrl,isRelative }) => {
 			sort-endpoints-by="summary"
             regular-font="Poppins"
             primary-color="#63C2C7"
-            allow-server-selection="false"
+            allow-server-selection="true"
             server-url={serverUrl}
             default-api-server={serverUrl}
             show-header="false"
