ae8376419eb50693c0e02b7b32ab4674591abc6a
NicolasPeyrusse <nicolas.peyrusse@xpollens.com>
2024-12-17 18:32:20 +0100

    Update sidebars. - add individual entrepreneur

--- sidebars.js
+++ sidebars.js
@@ -43,7 +43,7 @@ module.exports = {
     {
       type: 'category',
       label: 'Users',
-      items: ['api/KYC','api/Users'],
+      items: ['api/KYC','api/Users','api/IndividualEntrepreneur'],
     },
     {
       type: 'category',
